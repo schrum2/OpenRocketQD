de8893b8e0dcc7d88182d741cb9816a150ba839b
Jacob Schrum <schrum2@southwestern.edu>
2024-05-18 16:14:49 -0500

    First shot at helper function for evaluation
    Note sure if this works, but it is meant to simulate a rocket and return relevant information

---
+++ rocket_evaluate.py
@@ -0,0 +1,57 @@
+
+def simulate_rocket(sim, pts, rocket):
+    """
+        Simulate the rocket and return performance information.
+
+        sim     -- Rocket simulation object
+        opts    -- Simulation options
+    """
+    #from net.sf.openrocket.util import Coordinate # Once the instance starts, Java classes can be imported using JPype
+    #from net.sf.openrocket.masscalc import BasicMassCalculator
+    #from net.sf.openrocket.masscalc import MassCalculator
+    #from net.sf.openrocket.aerodynamics import WarningSet
+    #from net.sf.openrocket.aerodynamics import BarrowmanCalculator
+    #from net.sf.openrocket.aerodynamics import FlightConditions
+
+    #conf = rocket.getDefaultConfiguration() # Is this the actual simulation config though?
+    conf = sim.getConfiguration() # Or is this the right configuration?
+    bmc = BasicMassCalculator()
+    mct = MassCalculator.MassCalcType.LAUNCH_MASS
+
+    # Look at updateExtras under https://github.com/openrocket/openrocket/blob/7a9bb436c1ed91335a396693eccfc400ffe236d2/swing/src/main/java/info/openrocket/swing/gui/scalefigure/RocketPanel.java
+    adc = BarrowmanCalculator()
+    conds = FlightConditions(conf)
+    #conds.setMach(cpMach) # how do I get cpMach?
+    #conds.setAOA(cpAOA) # how do I get cpAOA?
+    #conds.setRollRate(cpRoll) # how do I get cpRoll?
+    #conds.setTheta(cpTheta) # how do I get cpTheta?
+    warnings = WarningSet()
+    warnings.clear()
+
+    # For BC/measures
+    cg = bmc.getCG(conf, mct).x
+    cp = adc.getCP(conf, conds, warnings).x
+
+    orh.run_simulation(sim)
+    data = orh.get_timeseries(sim, [FlightDataType.TYPE_TIME, FlightDataType.TYPE_ALTITUDE, FlightDataType.TYPE_VELOCITY_Z]) )
+    events = orh.get_events(sim) 
+
+    # Make a custom plot of the simulation
+
+    events_to_annotate = {
+        #FlightEvent.BURNOUT: 'Motor burnout',
+        FlightEvent.APOGEE: 'Apogee' #,
+        #FlightEvent.LAUNCHROD: 'Launch rod clearance'
+    }
+
+    apogee = 0.0 # Default
+    index_at = lambda t: (np.abs(data[FlightDataType.TYPE_TIME] - t)).argmin()
+    for event, times in events[i].items():
+        if event not in events_to_annotate:
+            continue
+        for time in times:
+            apogee = data[FlightDataType.TYPE_ALTITUDE][index_at(time)]
+
+    stability = cp - cg
+
+    return (stability, apogee)
