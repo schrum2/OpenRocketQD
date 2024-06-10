08e853cb03d630d7bda4141cff59939e2d954539
Jacob Schrum <schrum2@southwestern.edu>
2024-06-10 18:17:23 -0500

    Lowered max altitude
    Can't achieve much beyond 50 when body tube is more limited.

--- evolve_rockets.py
+++ evolve_rockets.py
@@ -661,7 +661,7 @@ def create_scheduler(config, algorithm, seed=None):
     MIN_STABILITY = 1.0
     MAX_STABILITY = 3.0
     MIN_ALTITUDE = 0.0
-    MAX_ALTITUDE = 200.0 # Should this allow higher?
+    MAX_ALTITUDE = 70.0 # With fixed body tube, max altitude is lower
     
     bounds = [(MIN_STABILITY, MAX_STABILITY), (MIN_ALTITUDE, MAX_ALTITUDE)]
     initial_sol = np.array(STARTING_SOLUTION)
