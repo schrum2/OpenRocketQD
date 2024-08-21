892bfc5ff00f1e0d6eed2e0db915c58fe0428a81
Jacob Schrum <schrum2@southwestern.edu>
2024-08-20 23:48:19 -0500

    batch files for running multiple experiments
    Made by ChatGPT. Runs a given evolution command a certain number of times, based on command line parameter.

---
+++ evolve_stabilitynose_altitude_map_elites.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Check if N is provided as a command line parameter
+if "%~1"=="" (
+    echo Please provide the value of N as a command line parameter.
+    exit /b 1
+)
+
+set N=%~1
+
+for /L %%i in (0,1,%N%-1) do (
+    python evolve_rockets.py map_elites %%i stabilitynose_altitude
+)
+
+endlocal
