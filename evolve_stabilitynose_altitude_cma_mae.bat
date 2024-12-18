d5f00da2f8f822a9107f2d8c80fea15325dbbc2c
Jacob Schrum <schrum2@southwestern.edu>
2024-12-18 13:57:42 -0600

    Added batch file to run CMA-MAE experiments
    Seems like CMA-MAE is considered better than CMA-ME, so I guess I should have run this in the first place. Do we will have time to incorporate results from this into the paper?

---
+++ evolve_stabilitynose_altitude_cma_mae.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if "%~1"=="" (
+    echo Please provide the value of N as starting experiment number.
+    exit /b 1
+)
+
+if "%~2"=="" (
+    echo Please provide the value of M as ending experiment number.
+    exit /b 1
+)
+
+set N=%~1
+set M=%~2
+
+for /L %%i in (%N%,1,%M%) do (
+    python evolve_rockets.py cma_mae %%i stabilitynose_altitude
+)
+
+endlocal
